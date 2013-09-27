daa609851df53c48b36f78cc82b397ddddf5f7b9
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2013-09-27 18:41:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecretSanta::Application.config.secret_key_base = '5dc7c1dab8367e6f32dbfd43c5ec76e9ef7f34cc66aa5a55dcf79e40deb8518ff01de430a903ac8f6d68284a8ad3887e16051848f1a06d9a3ccd3e631eaa42e8'
