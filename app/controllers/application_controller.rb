b84e5b4395623463d123d2e97ad5c4f0ddc51471
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2013-09-30 21:54:24 -0400

    setting up controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when ".csv"   then Roo::Csv.new(file.path, nil, :ignore)
+    when ".xls"   then Roo::Excel.new(file.path, nil, :ignore)
+    when ".xlsx"  then Roo::Excelx.new(file.path, nil, :ignore)
+    else
+      raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
+  def random_password
+    #do random password here
+  end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,5 +5,17 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.create
+    spreadsheet = open_spreadsheet(params[:file])
+    uploaded_users = spreadsheet.split('\n')
+    uploaded_users.each do |u|
+      splits = u.split(',')
+      @group.users.create(
+        first_name: splits[0],
+         last_name: splits[1],
+             email: splits[2],
+          password: random_password
+      )
+
+    end
   end
 end
