25f1d7c9bde08fe4708b2e3bf21fa4c8605bcc98
Tom Kiefhaber <tom.kiefhaber@hungrymachine.com>
2013-10-05 11:19:02 -0400

    remove test unit and users controller

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @group = Group.find(params[:id])
-    @pair  = @group.pairs.where(giver_id: params[:giver_id])
-    @giver = User.find(@pair.giver_id)
-    @receiver = User.find(@pair.receiver_id)
-  end
-end
